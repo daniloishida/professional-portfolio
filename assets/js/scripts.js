0278659ba51da145da119303813c0fa3983d0cda
Danilo Issida Gonçalves <62553509+daniloishida@users.noreply.github.com>
2025-02-27 11:41:48 -0300

    Alteracao de paginas

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -87,3 +87,10 @@ document.addEventListener('DOMContentLoaded', function() {
     // Set initial language
     toggleLanguage('pt');
 });
+
+function showSection(sectionId) {
+    document.querySelectorAll('main > section').forEach(section => {
+        section.classList.add('hidden');
+    });
+    document.getElementById(sectionId).classList.remove('hidden');
+}
